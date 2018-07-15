b489da3e890c481f534e3d785c2653375f11ce04
george redinger <george@georgeredinger.com>
2018-07-15 15:23:22 -0700

    indexdDB to firestore

--- public/app.js
+++ public/app.js
@@ -1,78 +1,99 @@
  //app.js
-  let saveId = document.getElementById("save");
-  saveId.onclick = saveForm;
+ let saveId = document.getElementById("save");
+ saveId.onclick = saveForm;
 
-  let loadId = document.getElementById("load");
-  loadId.onclick = loadForm;
+ let loadId = document.getElementById("load");
+ loadId.onclick = loadForm;
 
-  function saveForm() {
-    let textId = document.getElementById("notes");
-    let siteId = document.getElementById("siteId");
-    let siteNumber = siteId.value;
-    let notes = textId.value;
-    let date = new Date();
-    var timeStamp = date.toISOString();
+ function updateOfflineStatus(s) {
+   let SiteNoteInfoId = document.getElementById("noteInfo");
+   s.forEach(function (site) {
+     var checkBox = document.createElement("input");
+     var label = document.createElement("label");
+     checkBox.type = "checkbox";
+     checkBox.value = site;
+     SiteNoteInfoId.appendChild(checkBox);
+     SiteNoteInfoId.appendChild(label);
+     label.appendChild(document.createTextNode(site));
+   });
+ }
 
 
-    // if offline and unauthenticated
-    if(theUser == null){
-      theUser = {}; 
-      theUser.email = 'offline.email.com';
-    }
-    let saveme = {
-      siteId: siteNumber,
-      notes: `${timeStamp}
+
+ function saveForm() {
+   let textId = document.getElementById("notes");
+   let siteId = document.getElementById("siteId");
+   let siteNumber = siteId.value;
+   let notes = textId.value;
+   let date = new Date();
+   var timeStamp = date.toISOString();
+
+
+   // if offline and unauthenticated
+   if (theUser == null) {
+     theUser = {};
+     theUser.email = 'offline.email.com';
+   }
+   let saveme = {
+     siteId: siteNumber,
+     notes: `${timeStamp}
     [${location.lat},${location.lng}]
      ${theUser.email}
      ${notes}`
-    };
-    notesRef.doc(siteNumber).set(saveme);
-    textId.value = saveme.notes;
-    offlinePut(siteNumber, saveme);
+   };
+   notesRef.doc(siteNumber).set(saveme);
+   textId.value = saveme.notes;
+   offlinePut(siteNumber, saveme);
    // function storeSite(siteId, data)
-  }
+ }
 
-  function loadForm() {
-    let siteId = document.getElementById("siteId");
-    let siteNumber = siteId.value;
-    let textId = document.getElementById("notes");
+ function loadForm() {
+   let siteId = document.getElementById("siteId");
+   let siteNumber = siteId.value;
+   let textId = document.getElementById("notes");
+   let authed = theUser != null;
+   // if offline and unauthenticated
 
-    loadSite(siteNumber).then(function (doc) {
-// if offline and unauthenticated
+   if (authed) {
+     loadSite(siteNumber).then(function (doc) {
+       if (doc.exists) {
+         console.log("Document data:", doc.data());
+         let d = doc.data();
+         console.log("read :", d);
+         textId.value = d.notes;
+       } else {
+        offlineGet(siteNumber).then(function (doc) {
+          textId.value = doc.notes;
+        }).catch(function (error) {
+          textId.value = 'its all your notes now';
+        });
+       }
+     }).catch(function (error) {
+       console.log("Error getting document:", error);
+     });
+   } else {
+     offlineGet(siteNumber).then(function (doc) {
+       textId.value = doc.notes;
+     }).catch(function (error) {
+       textId.value = "all your offline notes";
+     });
+   }
 
 
+ }
 
-      if (doc.exists) {
-        console.log("Document data:", doc.data());
-        let d = doc.data();
-        console.log("read :", d);
-        textId.value = d.notes;
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-        textId.value = 'its all your notes now';
+ function success(ret) {
+   console.log("success ", ret);
+ }
 
-      }
-    }).catch(function (error) {
-      console.log("Error getting document:", error);
-    });
-       // function loadSite(siteId, data)
+ function failure(ret) {
+   console.log("failure ", ret);
 
-  }
+ }
 
-  function success(ret) {
-    console.log("success ", ret);
-  }
-
-  function failure(ret) {
-    console.log("failure ", ret);
-
-  }
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(
-      'sw.js', {}
-    ).then(success, failure);
-  }
-  console.log(theUser);
- 
+ if ('serviceWorker' in navigator) {
+   navigator.serviceWorker.register(
+     'sw.js', {}
+   ).then(success, failure);
+ }
+ console.log(theUser);
--- public/offlineDB.js
+++ public/offlineDB.js
@@ -6,17 +6,19 @@ if (!window.indexedDB) {
 var dbPromise;
 var keyValStore;
 var offlineStore;
-var siteIndex;
+// var siteIndex;
 var dbName = 'realNotes';
 
-var dbPromise = idb.open(dbName, 1, function (upgradeDb) {
-  if (upgradeDb.oldVersion == 0) {
-    noteStore = upgradeDb.createObjectStore(dbName);
-    noteStore.createIndex(dbName, 'siteId');
-  } else {
-    noteStore = upgradeDb.transaction.objectStore(dbName);
-  }
-  siteIndex = noteStore.createIndex(dbName, 'siteId');
+var dbPromise = idb.open(dbName, 1,
+  function (upgradeDb) {
+    upgradeDb.createObjectStore(dbName);
+    notestore = upgradeDb.transaction.objectStore(dbName);
+    notestore.createIndex('siteId', dbName);
+    offlineGetAll();
+  }).then(function (db) {
+  console.log(`DB ${dbName} opened ${dbPromise}`);
+  offlineGetAll(dbPromise);
+
 });
 
 
@@ -30,17 +32,27 @@ function offlineDBclose() {}
 function offlineDBCount() {}
 
 function offlineGet(site) {
-  dbPromise.then(function (db) {
+  return idb.open(dbName, 1, upgradeDB => {
+    upgradeDB.createObjectStore(dbName);
+    switch (upgradeDB.oldVersion) {
+      case 0:
+        upgradeDB.createObjectStore(dbName);
+    }
+  }).then(db => {
     var tx = db.transaction(dbName);
     var keyValStore = tx.objectStore(dbName);
     return keyValStore.get(site);
-  }).then(function (val) {
-    console.log('The value of "site" is:', val);
   });
 }
 
 function offlinePut(site, object) {
-  dbPromise.then(function (db) {
+  idb.open(dbName, 1, upgradeDB => {
+    upgradeDB.createObjectStore(dbName);
+    switch (upgradeDB.oldVersion) {
+      case 0:
+        upgradeDB.createObjectStore(dbName);
+    }
+  }).then(db => {
     var tx = db.transaction(dbName, 'readwrite');
     var keyValStore = tx.objectStore(dbName);
     keyValStore.put(object, site);
@@ -48,117 +60,28 @@ function offlinePut(site, object) {
   }).then(function () {
     console.log('Added', site, 'to ', object);
   });
-  dbPromise.then(db => {
-    return db.transaction(dbName).objectStore(dbName).getAll();
-  }).then(function (allObjs) {
-    console.log("saved keys ",allObjs);
-    console.log("saved key count ",allObjs.length);
-    console.log("if online, shoot them to firestore");
-    console.log("if just gone online, verify that they should be sent to firestore");
-    //after each site sent to firestore, delete the local storage for that site
-  });
 }
 
 
 function offlineRm() {}
 
+function offlineGetAll(dbPromise) {
+  idb.open(dbName, 1, upgradeDB => {
+    upgradeDB.createObjectStore(dbName);
+    switch (upgradeDB.oldVersion) {
+      case 0:
+        upgradeDB.createObjectStore(dbName);
+    }
+  }).then(db => {
+    const tx = db.transaction(dbName);
+    const store = tx.objectStore(dbName);
+    const keys = [];
 
-
-
-
-
-
-
-
-
-
-
-
-// set "foo" to be "bar" in "keyval"
-
-
-// dbPromise.then(function(db) {
-//   var tx = db.transaction('keyval', 'readwrite');
-//   var keyValStore = tx.objectStore('keyval');
-//   keyValStore.put('cat', 'favoriteAnimal');
-//   return tx.complete;
-// }).then(function() {
-//   console.log('Added favoriteAnimal:cat to keyval');
-// });
-
-// // add people to "people"
-// dbPromise.then(function(db) {
-//   var tx = db.transaction('people', 'readwrite');
-//   var peopleStore = tx.objectStore('people');
-
-//   peopleStore.put({
-//     name: 'Sam Munoz',
-//     age: 25,
-//     favoriteAnimal: 'dog'
-//   });
-
-//   peopleStore.put({
-//     name: 'Susan Keller',
-//     age: 34,
-//     favoriteAnimal: 'cat'
-//   });
-
-//   peopleStore.put({
-//     name: 'Lillie Wolfe',
-//     age: 28,
-//     favoriteAnimal: 'dog'
-//   });
-
-//   peopleStore.put({
-//     name: 'Marc Stone',
-//     age: 39,
-//     favoriteAnimal: 'cat'
-//   });
-
-//   return tx.complete;
-// }).then(function() {
-//   console.log('People added');
-// });
-
-// // list all cat people
-// dbPromise.then(function(db) {
-//   var tx = db.transaction('people');
-//   var peopleStore = tx.objectStore('people');
-//   var animalIndex = peopleStore.index('animal');
-
-//   return animalIndex.getAll('cat');
-// }).then(function(people) {
-//   console.log('Cat people:', people);
-// });
-
-// // people by age
-// dbPromise.then(function(db) {
-//   var tx = db.transaction('people');
-//   var peopleStore = tx.objectStore('people');
-//   var ageIndex = peopleStore.index('age');
-
-//   return ageIndex.getAll();
-// }).then(function(people) {
-//   console.log('People by age:', people);
-// });
-
-// // Using cursors
-// dbPromise.then(function(db) {
-//   var tx = db.transaction('people');
-//   var peopleStore = tx.objectStore('people');
-//   var ageIndex = peopleStore.index('age');
-
-//   return ageIndex.openCursor();
-// }).then(function(cursor) {
-//   if (!cursor) return;
-//   return cursor.advance(2);
-// }).then(function logPerson(cursor) {
-//   if (!cursor) return;
-//   console.log("Cursored at:", cursor.value.name);
-//   // I could also do things like:
-//   // cursor.update(newValue) to change the value, or
-//   // cursor.delete() to delete this entry
-//   return cursor.continue().then(logPerson);
-// }).then(function() {
-//   console.log('Done cursoring');
-// });
+    store.getAllKeys().
+    then(
+      function (result) {
+        updateOfflineStatus(result);
+      }
+    );
+  });
+}
